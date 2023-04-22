07b00b9ee1d9083f176f04cabcdea4a7d2342867
thefakejj <jasper.laurila@gmail.com>
2023-04-22 22:48:27 +0300

    moved all files from src to root for fly's sake

---
+++ db.py
@@ -0,0 +1,8 @@
+from os import getenv
+from flask_sqlalchemy import SQLAlchemy
+from app import ow_app
+
+ow_app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+#ow_app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
+
+ow_db = SQLAlchemy(ow_app)
