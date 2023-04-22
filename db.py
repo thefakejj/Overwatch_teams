f0d6647961126fe6afebb4812085c2eb9019581f
thefakejj <jasper.laurila@gmail.com>
2023-04-23 00:56:04 +0300

    local install still functional

--- db.py
+++ db.py
@@ -3,6 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from app import ow_app
 
 #ow_app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-ow_app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
+ow_app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("gres", "gresql", 1)
 
 ow_db = SQLAlchemy(ow_app)
