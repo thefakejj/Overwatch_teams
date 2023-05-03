72180b9e855d7be46b9ebf5245b15acf9a61463e
thefakejj <jasper.laurila@gmail.com>
2023-05-03 23:31:58 +0300

    sql injection removed from db_search_functions

--- db_search_functions.py
+++ db_search_functions.py
@@ -2,7 +2,13 @@ from sqlalchemy.sql import text
 from db import ow_db
 
 def show_all_players():
-    sql = text(f"SELECT people.name, teams.name, countries.name, people.status FROM people, teams, people_teams_roles, countries WHERE people.id = people_teams_roles.person_id AND people_teams_roles.player_team = teams.id AND people.country_id = countries.id")
+    sql = text('''
+        SELECT people.name, teams.name, countries.name, people.status
+        FROM people, teams, people_teams_roles, countries
+        WHERE people.id = people_teams_roles.person_id
+        AND people_teams_roles.player_team = teams.id
+        AND people.country_id = countries.id
+        ''')
     result = ow_db.session.execute(sql)
     selection = result.fetchall()
     return selection
@@ -12,8 +18,15 @@ def searching_player_name(input):
         selection = show_all_players()
     else:
         input = input.lower()
-        sql = text(f"SELECT people.name, teams.name, countries.name, people.status FROM people, teams, people_teams_roles, countries WHERE people.id = people_teams_roles.person_id AND people_teams_roles.player_team = teams.id AND people.country_id = countries.id AND LOWER(people.name) LIKE '%{input}%'")
-        result = ow_db.session.execute(sql)
+        sql = text('''
+            SELECT people.name, teams.name, countries.name, people.status
+            FROM people, teams, people_teams_roles, countries
+            WHERE people.id = people_teams_roles.person_id
+            AND people_teams_roles.player_team = teams.id
+            AND people.country_id = countries.id
+            AND LOWER(people.name) LIKE '%:input%'
+        ''')
+        result = ow_db.session.execute(sql, {"input":input})
         selection = result.fetchall()
     return selection
 
